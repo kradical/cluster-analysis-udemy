19f27ce485187b6a415bcb296c6bf1dec0d8ac2d
Konrad Schultz <konrad_here@hotmail.com>
2018-11-25 22:22:50 -0700

    Add data and exercise1.

---
+++ src/prereq/exercise1.py
@@ -0,0 +1,23 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+A = np.array([
+    [0.3, 0.6, 0.1],
+    [0.5, 0.2, 0.3],
+    [0.4, 0.1, 0.5],
+])
+
+v = np.array([1/3, 1/3, 1/3])
+
+data = []
+
+for i in range(25):
+    vn = v * A
+
+    norm = np.linalg.norm(vn - v)
+    data.append(norm)
+
+    v = vn
+
+plt.plot(data)
+plt.show()
