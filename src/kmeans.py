e2101bdb08ae3b9ed0ed8c4c1c488e3a75a1b7c5
Konrad Schultz <konrad_here@hotmail.com>
2018-11-29 01:31:56 -0700

    Refactor kmeans for readability.

--- src/kmeans.py
+++ src/kmeans.py
@@ -1,17 +1,21 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def d(u, v):
+def squareDistance(u, v):
     diff = u - v
     return diff.dot(diff)
 
-def cost(X, R, M):
+# We want to minimize intra-cluster variance (squard error)
+def objective(X, R, M):
     cost = 0
     for k in range(len(M)):
         for n in range(len(X)):
-            cost += R[n,k]*d(M[k], X[n])
+            cost += R[n,k] * squareDistance(M[k], X[n])
 
     return cost
+
+def expDistance(p1, p2, beta):
+    return np.exp(-beta * squareDistance(p1, p2))
 
 def plot_k_means(X, K, max_iter=20, beta=1.0):
     N, D = X.shape
@@ -24,30 +28,37 @@ def plot_k_means(X, K, max_iter=20, beta=1.0):
 
     costs = np.zeros(max_iter)
     for i in range(max_iter):
-        # recalculate responsibility
+        # calculate responsibility for each point for each cluster
         for k in range(K):
             for n in range(N):
-                denomIter = (np.exp(-beta * d(M[j], X[n])) for j in range(K))
-                numExp = -beta * d(M[k], X[n])
+                sumMeanDistances = np.array([
+                    expDistance(M[j], X[n], beta) for j in range(K)
+                ]).sum()
 
-                R[n,k] = np.exp(numExp) / np.sum(np.fromiter(denomIter, float))
+                meanDistance = expDistance(M[k], X[n], beta)
 
-        # recalculate the means
+                softMax = meanDistance / sumMeanDistances
+
+                R[n,k] = softMax
+
+        # calculate the means for each cluster
         for k in range(K):
             M[k] = R[:,k].dot(X) / R[:,k].sum()
 
         # calculate cost and break if it hasn't changed much
-        costs[i] = cost(X, R, M)
-        if i > 0 and np.abs(costs[i] - costs[i-1]) < 0.1:
+        costs[i] = objective(X, R, M)
+        if i > 0 and np.abs(costs[i] - costs[i - 1]) < 0.1:
             break
 
+    # pick K random RGB values to represent clusters
     random_colors = np.random.random((K, 3))
+    # weight the color of a point based on its responsibility to each cluster.
     colors = R.dot(random_colors)
 
     plt.scatter(X[:,0], X[:,1], c=colors)
     plt.show()
 
-def main():
+def generate_samples():
     D = 2
     s = 4 # distance between means
 
@@ -62,6 +73,11 @@ def main():
     data[:300, :] = np.random.randn(300, D) + mean1
     data[300:600, :] = np.random.randn(300, D) + mean2
     data[600:, :] = np.random.randn(300, D) + mean3
+
+    return data
+
+def main():
+    data = generate_samples()
 
     clusters = 3
     plot_k_means(data, clusters)
